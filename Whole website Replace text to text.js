e78bcc3da2d0c9723a9f640c7697fd5d063c4872
Siddharth Pandey <134340488+sidp121@users.noreply.github.com>
2023-09-16 04:44:55 +0530

    Update Whole website Replace text to text.js

--- Whole website Replace text to text.js
+++ Whole website Replace text to text.js
@@ -1,3 +1,4 @@
+// Whole website Replace text to text
 jQuery(document).ready(function() {
     // Use a function to traverse all text nodes
     function replaceTextInNode(node) {
@@ -20,3 +21,14 @@ jQuery(document).ready(function() {
     // Start the text replacement process from the body element
     replaceTextInNode(document.body);
 });
+
+
+ // Replace "Lifepro" with "<span class='lifepro-mark'>Lifepro</span>" in all text nodes
+
+        jQuery(document).ready(function() {
+            jQuery("body").find("*").addBack().contents().filter(function() {
+                return this.nodeType === 3; // Filter for text nodes
+            }).each(function() {
+                jQuery(this).replaceWith(jQuery(this).text().replace(/Lifepro/g, "<span class='lifepro-mark'>Lifepro</span>"));
+            });
+  });
